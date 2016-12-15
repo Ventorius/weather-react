a5a52bd228dc41d9f653e679d15486e3d45a575b
Sebastian Basznianin <s.basznianin@konmex.com>
2016-12-15 11:57:33 +0100

    Changes

--- server.js
+++ server.js
@@ -3,17 +3,17 @@ const express = require('express');
 var app = express();
 
 const PORT = process.env.PORT || 3000;
-
-app.use(function(req, res, next) {
-
-    //openmap uzywa tylko http
-    // if (req.headers['x-forwarded-proto'] = 'http') {
-    //     next();
-    // } else {
-    //     res.redirect('http://' + req.hostname + req.url);
-    // }
-
-})
+// 
+// app.use(function(req, res, next) {
+//
+//     openmap uzywa tylko http
+//     if (req.headers['x-forwarded-proto'] = 'http') {
+//         next();
+//     } else {
+//         res.redirect('http://' + req.hostname + req.url);
+//     }
+//
+// })
 
 
 app.use(express.static('public'));
